d205365eee24417f8f9002ac1986e8b514bcd915
aleksandr-litvinov <litvinov1618@gmail.com>
2021-11-04 13:19:06 +0200

    Add holidays to vacation calculator

--- src/dayjs.ts
+++ src/dayjs.ts
@@ -1,8 +1,25 @@
 import dayjs from 'dayjs'
-import dayjsBusinessDays from 'dayjs-business-days'
+import dayjsBusinessDays from '@backtolife/dayjs-business-days'
 import 'dayjs/locale/uk.js'
 
-dayjs.extend(dayjsBusinessDays)
+const options = {
+    holidays: [
+        '2021-01-01',
+        '2021-01-07',
+        '2021-03-08',
+        '2021-05-02',
+        '2021-05-03',
+        '2021-05-10',
+        '2021-06-20',
+        '2021-06-28',
+        '2021-08-24',
+        '2021-10-14',
+        '2021-12-27',
+    ],
+    holidayFormat: 'YYYY-MM-DD',
+}
+
+dayjs.extend(dayjsBusinessDays, options)
 
 dayjs.locale('uk')
 
